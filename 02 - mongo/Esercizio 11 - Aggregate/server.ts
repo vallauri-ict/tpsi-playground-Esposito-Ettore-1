0a6395ef3405b64a3b6b556fc4dfe99b980c1c47
unknown <e.esposito.1520@vallauri.edu>
2021-11-23 21:49:45 +0100

    Fine esercizi mongo

--- 02 - mongo/Esercizio 11 - Aggregate/server.ts
+++ 02 - mongo/Esercizio 11 - Aggregate/server.ts
@@ -222,3 +222,199 @@ mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 8
     else
         console.log("Query 8", err.message);
 });
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 9
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Orders");
+        let request = collection.aggregate([
+            { $project : { status : 1, nDettagli : 1 } }, 
+            { $unwind : "$nDettagli" }, 
+            { $group : { _id : "$status",  sommaDettagli : { $sum : "$nDettagli" } } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 9", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 9", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 9", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 10
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Students");
+        let request = collection.aggregate([
+            { $match : { "$expr" : { "$eq" : [ { "$year" : "$nato" } , 2000 ] } } },
+            { $project : { "nome" : 1, "genere" : 1, "classe" : 1, "nato" : 1 } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 10", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 10", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 10", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 11
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Unicorns");
+        let request = collection.aggregate([
+            { $group : { "_id" : { "gender" : "$gender" }, "avgWeight" : { $avg : "$weight" } } },
+            { $sort : { avgWeight : -1 } },
+            { $project : { "gender" : 1, "avgWeight" : { $round : [ "$avgWeight", 2 ] } } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 11", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 11", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 11", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 12
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Unicorns");
+        let request = collection.aggregate([
+            { $match : { "loves" : "apple" } },
+            { $group : { "_id" : { "gender" : "$gender" }, "vampires" : { $sum : "$vampires" } } },
+            { $sort : { "vampires" : -1 } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 12", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 12", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 12", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 13
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Unicorns");
+        let request = collection.aggregate([
+            { $project : { "_id" : 0, "loves" : 1 } },
+            { $unwind : "$loves" },
+            { $group : { "_id" : { "loves" : "$loves" }, "tot" : { $sum : 1 } } },
+            { $sort : { "tot" : -1 } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 13", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 13", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 13", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 14
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Students");
+        let request = collection.aggregate([
+            { $project : { "_id" : 0, "classe" : 1, "media" : { $avg : "$voti" } } },
+            { $group : { "_id" : { "classe" : "$classe" } , "media" : { $avg : "$media" } } },
+            { $match : { "media" : { $gte : 6 } } },
+            { $sort : { "media" : -1 } },
+            { $project : { "classe" : 1, "media" : { $round : [ "$media", 2 ] } } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 14", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 14", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 14", err.message);
+});
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 15
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Students");
+        let lastDate :Date = new Date();
+        lastDate.setFullYear(new Date().getFullYear() - 18);
+        let request = collection.aggregate([
+            { $match : { "nato" : { $gt : lastDate } } },
+            { $project : { "nome" : 1, "genere" : 1, "classe" : 1, "nato" : 1 } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 15", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 15", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 15", err.message);
+})
+
+mongoClient.connect(CONNECTIONSTRING, function(err, client){ //query 16
+    if(!err)
+    {
+        let db = client.db(dbName);
+        let collection = db.collection("Students");
+        let lastDate :Date = new Date();
+        lastDate.setFullYear(new Date().getFullYear() - 18);
+        let request = collection.aggregate([
+            { $match : { "$expr" : { "$eq" : [ { "$year" : "$nato" } , 2000 ] } } },
+            { $project : { "nome" : 1, "genere" : 1, "classe" : 1, "nato" : 1 } }
+        ]).toArray();
+        request.then(function (data) {
+            console.log("Query 16", data);
+        });
+        request.catch(function (err) {
+            console.log("Query 16", err);
+        });
+        request.finally(function () {
+            client.close();
+        });
+    }
+    else
+        console.log("Query 16", err.message);
+});
